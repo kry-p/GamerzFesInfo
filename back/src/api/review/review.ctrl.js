112e501baca93ea958f7d90fceb9f27d00bfc1eb
kry-p <psd7256@gmail.com>
2021-04-29 12:45:58 +0900

    Add filter feature
    취소되거나 심의거부된 게임물을 숨기는 필터를 구현하였습니다.

--- back/src/api/review/review.ctrl.js
+++ back/src/api/review/review.ctrl.js
@@ -1,12 +1,26 @@
 import Review from '../../models/review';
 import { dateToString, stringToDate } from '../../modules/date';
 
+const rejected = new RegExp('reject');
+const cancelled = new RegExp('cancel');
+
+const boolConverter = (string) => {
+  try {
+    if (string === 'true') return true;
+    else return false;
+  } catch (error) {
+    return null;
+  }
+};
+
 /*
     심의결과 조회 (날짜 기준, 페이지당 20개씩)
     GET /api/review/listbydate
 */
 export const listbydate = async (ctx) => {
   const page = parseInt(ctx.query.page || '1', 10); // 입력값이 없을 경우 기본 페이지는 1
+  const cancel = boolConverter(ctx.query.cancel);
+  const reject = boolConverter(ctx.query.reject);
   let startDate = stringToDate(ctx.query.startdate);
   let endDate = stringToDate(ctx.query.enddate);
 
@@ -33,12 +47,22 @@ export const listbydate = async (ctx) => {
   }
 
   try {
+    const notIn = [];
+
+    if (!reject) notIn.push(rejected);
+    if (!cancel) notIn.push(cancelled);
+
     const review = await Review.find(filter)
+      .where('rating')
+      .nin(notIn)
       .sort({ date: -1 }) // 날짜 역순
       .limit(10)
       .skip((page - 1) * 10)
       .exec();
-    const reviewCount = await Review.countDocuments(filter).exec();
+    const reviewCount = await Review.countDocuments(filter)
+      .where('rating')
+      .nin(notIn)
+      .exec();
     ctx.set('LastPage', Math.ceil(reviewCount / 10));
     ctx.set('CurrentPage', page);
     ctx.set('StartDate', dateToString(startDate));
@@ -55,6 +79,8 @@ export const listbydate = async (ctx) => {
 */
 export const listbykeyword = async (ctx) => {
   const page = parseInt(ctx.query.page || '1', 10); // 입력값이 없을 경우 기본 페이지는 1
+  const cancel = boolConverter(ctx.query.cancel);
+  const reject = boolConverter(ctx.query.reject);
   const keyword = ctx.query.keyword;
   const regex = new RegExp(keyword);
 
@@ -76,12 +102,22 @@ export const listbykeyword = async (ctx) => {
   }
 
   try {
+    const notIn = [];
+
+    if (!reject) notIn.push(rejected);
+    if (!cancel) notIn.push(cancelled);
+
     const review = await Review.find(filter)
-      .sort({ date: -1 }) // 날짜 역순
+      .where('rating')
+      .nin(notIn)
+      .sort({ date: -1 })
       .limit(10)
       .skip((page - 1) * 10)
       .exec();
-    const reviewCount = await Review.countDocuments(filter).exec();
+    const reviewCount = await Review.countDocuments(filter)
+      .where('rating')
+      .nin(notIn)
+      .exec();
     ctx.set('CurrentPage', page);
     ctx.set('LastPage', Math.ceil(reviewCount / 10));
     ctx.set('Keyword', encodeURI(keyword));
